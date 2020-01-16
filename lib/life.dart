0c12508d4ef79e4c5847332e10671e7758f0c7a7
Eric Dallo <ericdallo06@hotmail.com>
2020-01-16 19:33:39 -0300

    #minor: first app working

--- lib/life.dart
+++ lib/life.dart
@@ -5,17 +5,14 @@ import 'package:flame/sprite.dart';
 import 'package:grube/game_controller.dart';
 
 const double _SIZE = 36;
-const int MAX_LIFES = 3;
 
 class Lifes {
   final GameController gameController;
   final List<Life> lifes = [];
 
   Lifes(this.gameController, int lifes) {
-    for (var i = 1; i <= MAX_LIFES; i++) {
-      String spriteName = lifes >= i ? 'full_life' : 'empty_life';
-
-      var life = Life(i * _SIZE, spriteName);
+    for (var i = 1; i <= lifes; i++) {
+      var life = Life(i * _SIZE);
 
       this.lifes.add(life);
       this.gameController.add(life);
@@ -32,14 +29,15 @@ class Lifes {
   }
 
   void hurt() {
-    this.lifes.removeLast();
+    this.lifes.lastWhere((life) => !life.hurted).hurt();
   }
 }
 
 class Life extends SpriteComponent {
   double xDec;
+  bool hurted = false;
 
-  Life(this.xDec, sprite) : super.square(_SIZE, sprite + '.png');
+  Life(this.xDec) : super.square(_SIZE, 'full_life.png');
 
   @override
   void resize(Size size) {
@@ -50,5 +48,10 @@ class Life extends SpriteComponent {
   @override
   bool isHud() {
     return true;
+  }
+
+  void hurt() {
+    this.hurted = true;
+    this.sprite = Sprite('empty_life.png');
   }
 }
